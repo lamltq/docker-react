35e9aa0559af65092c88ca487b2776a5d608726a
Admin <lelam2912@gmail.com>
2022-04-03 08:34:45 +0700

    add circleci config file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Change this line again.
+          Change here
         </p>
         <a
           className="App-link"
